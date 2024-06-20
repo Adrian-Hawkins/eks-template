7d7bd01cf542e4255fd2ae029da06b35a45aff4d
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-06-20 15:19:25 +0200

    a lot of changes

--- Terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-personal-adr"
-#     key    = "terraform.tfstate"
-#     region = "eu-west-1"
-#   }
-# }
--- example.services.user/main.js
+++ example.services.user/main.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/user/:id', (req, res) => {
+app.get('/get/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`Hello ${userId}`);
+});
+app.get('/other/user/:id', (req, res) => {
     const userId = req.params.id;
     res.send(`Hello ${userId}`);
 });
