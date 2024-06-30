13939214c6bb502dc70a45661a2a5eef1f101463
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-06-30 18:11:36 +0200

    ect

--- Terraform/ecr.tf
+++ Terraform/ecr.tf
@@ -1,8 +1,21 @@
-resource "aws_ecr_repository" "foo" {
-  name                 = "bar"
-  image_tag_mutability = "MUTABLE"
+# resource "aws_ecr_repository" "foo" {
+#   name                 = "bar"
+#   image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
+  
+# }
+resource "aws_ecrpublic_repository" "expressfoo" {
+
+  repository_name = "express"
+
+  catalog_data {
+    about_text        = "About Text"
+  }
+
+  tags = {
+    env = "production"
   }
 }
